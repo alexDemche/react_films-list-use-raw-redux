b76c12f168940bf27bedc5a27e3fa89dc0f12b1b
alexDemche <alexeyydemchenko@gmail.com>
2019-10-03 16:02:42 +0300

    build version

---
+++ precache-manifest.17d26f5193e53f25640ef8c9ea5adf63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d22dea62e699d7023f07297d969f387",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a7fdc2b9fb0e26366cf",
+    "url": "./static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "3ec19091a0bf5226dce5",
+    "url": "./static/css/main.ffa3e8f4.chunk.css"
+  },
+  {
+    "revision": "0a7fdc2b9fb0e26366cf",
+    "url": "./static/js/2.0142f2df.chunk.js"
+  },
+  {
+    "revision": "3ec19091a0bf5226dce5",
+    "url": "./static/js/main.e2b55b96.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
